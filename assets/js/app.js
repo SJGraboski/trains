817a9e9a609aac5ff9507541b86675969d0cdba8
Steven Graboski <sgrabosk@gmail.com>
2016-03-13 22:40:59 -0400

    data updates on page by the minutes of the actual hour

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,17 @@
-/* 1. Global Vars
+/* 1. Global Statements
  * ============== */
 
+// reference Firebase
 trainRef = new Firebase('https://sgtrains.firebaseio.com/');
+
+/* set up vars for later interval for every minute of the hour
+ * Source: http://stackoverflow.com/questions/23449917/
+ * run-js-function-every-new-minute */
+
+// get current seconds remaining in the current minute
+var time = new Date();
+var milliRemaining = (60 - time.getSeconds()) * 1000;
+
 
 /* 2. Functions
  * ============ */
@@ -37,19 +47,17 @@ var interface = {
 		var name = snapshot.val().name;
 		var dest = snapshot.val().dest;
 		var first = snapshot.val().first;
-		console.log(first);
 		var freq = snapshot.val().freq;
 
 		// catch current
-		m_current = moment();
+		var m_current = moment();
 
 		// make a string with only the current day
-		m_current_day = m_current.format('YYYY-MM-DD');
+		var m_current_day = m_current.format('YYYY-MM-DD');
 
 		// make first train moment
 		// using the first train's time along with the current day
-		m_first = moment(m_current_day + ' ' + first, 'YYYY-MM-DD hh:mm A');
-		console.log('first: ' + m_first.format('YYYY-MM-DD hh:mm A'));
+		var m_first = moment(m_current_day + ' ' + first, 'YYYY-MM-DD hh:mm A');
 
 		/* calculate when the next arrival time will be 
 		 * using freq, the m_current and m_first obj's, 
@@ -64,8 +72,8 @@ var interface = {
 		// add freq to m_arrival until it passes m_current.
 		// (this will give us the latest arrival time).
 		while(beforeCurrent) {
-			m_first.add(freq, 'minutes')
-			if (m_first.isAfter(m_current)) {
+			m_arrival.add(freq, 'minutes')
+			if (m_arrival.isAfter(m_current)) {
 				beforeCurrent = false;
 			}
 		}
@@ -86,17 +94,79 @@ var interface = {
 				)
 				.append( $('<td>')
 					.text(freq)
+					.addClass('freq-row')
 				)
 				.append( $('<td>')
 					.text(m_arrival.format('hh:mm A'))
+					.addClass('arrival-row')
 				)
 				.append( $('<td>')
 					.text(durationString)
+					.addClass('next-row')
 				)
 			)
 		;
+	},
+
+	trainMinute : function() {
+		// catch current time
+		var m_current = moment().tz("America/New_York");
+		// make a string with only the current day
+		var m_current_day = m_current.format('YYYY-MM-DD');
+
+		// catch the times and rates in the table, save as arr's
+		var freqs = $("td.freq-row").map(function() {
+									return Number($(this).text());
+							 	}).get();
+
+		var arrivals = $("td.arrival-row").map(function() {
+									return moment(m_current_day + " " + $(this).text(), 'YYYY-MM-DD hh:mm A').tz("America/New_York");
+							 	}).get();
+
+		var nexts = $("td.next-row").map(function() {
+									return Number($(this).text());
+							 	}).get();
+
+		// FOR LOOP
+		for (var i = 0; i < arrivals.length; i++) {
+
+			// if an arrival time falls after current time, major edits
+			if (arrivals[i].isBefore(m_current)) {
+
+				// this will stay true until m_arrival passes m_current
+				var beforeCurrent = true;
+
+				// make an arrival moment, which will morph in the while loop
+				var m_arrival = arrivals[i];
+
+				// keep adding to m_arrival until it's a valid arrival time
+				while(beforeCurrent) {
+					m_arrival.add(freqs[i], 'minutes')
+					if (m_arrival.isAfter(m_current)) {
+						beforeCurrent = false;
+					}
+				}
+
+				// edit the rows
+				var arrRow = $("td.arrival-row")[i];
+				$(arrRow).text(m_arrival.format("hh:mm A"));
+				var nextRow = $("td.next-row")[i]
+				$(nextRow).text(freqs[i]);
+			}
+			// if not, just change the next arrival change
+			else {
+
+				// bring down next num by a minute
+				nexts[i]--;
+
+				//edit the html
+				var nextRow = $("td.next-row")[i];
+				$(nextRow).text(nexts[i]);
+			}
+		}
 	}
 }
+
 
 /* 3. Calls
  * ======== */
@@ -104,14 +174,20 @@ var interface = {
 // submit button
 $('#train-form').on('click', '#submit', function() {
 	interface.trainSetter();
-	console.log("OK");
 	return false;
-})
+});
 
 // firebase updates
 trainRef.orderByChild("dateAdded").limitToLast(3)
 .on("child_added", function(childSnapshot, prevChildKey) {
 	interface.trainGetter(childSnapshot);
-})
+});
 
-// update site eve
+// set up a timeinterval using the actual minutes of the hour
+setTimeout(function() {
+	interface.trainMinute();
+	var intervalID = setInterval(function() { 
+		interface.trainMinute()
+	}, 60000)
+}, milliRemaining);
+
